1c2ea8195d470b75b77df60aa45e3285b1928936
shunsuke <shunnsuke21@gmail.com>
2014-05-18 23:00:24 +0900

    test

---
+++ ArrowsWorldCup/fuel/app/logs/2014/05/14.php
@@ -0,0 +1,5 @@
+<?php defined('COREPATH') or exit('No direct script access allowed'); ?>
+
+ERROR - 2014-05-14 00:26:32 --> Error - Cannot read file: "/Users/stsuru/Sites/ArrowsWorldCup/public//public/assets/json_model/companies.json", file does not exists. in /Users/stsuru/Sites/ArrowsWorldCup/fuel/core/classes/file.php on line 202
+ERROR - 2014-05-14 00:26:55 --> Error - Cannot read file: "/Users/stsuru/Sites/ArrowsWorldCup/public/assets/json_model/companies.json", file does not exists. in /Users/stsuru/Sites/ArrowsWorldCup/fuel/core/classes/file.php on line 202
+ERROR - 2014-05-14 00:47:00 --> Compile Error - Can't use function return value in write context in /Users/stsuru/Sites/ArrowsWorldCup/fuel/app/classes/controller/api.php on line 32
